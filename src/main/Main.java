1c52d1504e4ed72d8974b41cf79cac330eefbbbc
CezarGR <cgabriel.lourenzo@gmail.com>
2020-12-22 09:56:50 -0300

    Alterações

--- src/main/Main.java
+++ src/main/Main.java
@@ -18,25 +18,20 @@ public class Main {
 		
 		String name;
 		String model;
-		Integer license_plates;
+		String license_plates;
 		Integer hora;
 		Integer minuts;
 		
 		
 		
 		System.out.println();
-		hora = sc.nextInt();
-		
-		minuts = sc.nextInt();
-		
+	
 		System.out.print("Name: ");
 		name = sc.nextLine();
 		System.out.print("Car Model : ");
 		model = sc.nextLine();
 		System.out.print("Car license plates: ");
-		license_plates = sc.nextInt();
-		
-		Parking parking = new Parking(name, model, license_plates);
+		license_plates = sc.nextLine();
 		
 		System.out.println("time");
 		System.out.print("horas: ");
@@ -44,16 +39,26 @@ public class Main {
 		System.out.print("minuts: ");
 		minuts = sc.nextInt();
 		
-		parking.setHora(hora);
-		parking.setMinuts(minuts);
+		Parking parking = new Parking(name, model, license_plates, hora, minuts);
 		
 		
+		System.out.println("---------------------------");
+		System.out.println("1h - $10");
+		System.out.println("2h - $20");
+		System.out.println("3h - $30");
+		System.out.println("4h - $40");
+		System.out.println("---------------------------\n");
 		
+		System.out.println("What time is it?");
 		
+		System.out.println("time");
+		System.out.print("horas: ");
+		int hora1 = sc.nextInt();
+		System.out.print("minuts: ");
+		int minuts1 = sc.nextInt();
 		
-		
-		
-		
+		System.out.println("\n---------------------------\n");
+		System.out.println("Payment: $" + parking.calc(hora1, minuts1));
 		
 
 	}
--- src/parking/Parking.java
+++ src/parking/Parking.java
@@ -3,18 +3,48 @@ package parking;
 public class Parking {
 	private String name;
 	private String model;
-	private Integer license_plates;
+	private String license_plates;
 	private Integer hora;
 	private Integer minuts;
 	
 	public Parking() {
 	}
 	
-	public Parking(String name, String model, Integer license_plates) {
+	public Parking(String name, String model, String license_plates) {
 		this.name = name;
 		this.model = model;
 		this.license_plates = license_plates;
 	}
+		
+	public Parking(String name, String model, String license_plates, Integer hora, Integer minuts) {
+		this.name = name;
+		this.model = model;
+		this.license_plates = license_plates;
+		this.hora = hora;
+		this.minuts = minuts;
+	}
+
+	public double calc(int hora, int minuts) {
+		int h1;
+		int m1;
+		
+		h1 = hora-this.hora;
+		m1 = minuts-this.minuts;
+		
+		if(h1>4) {
+			return 40.00;
+		}
+		if(h1>=3) {
+			return 30.00;
+		}
+		if(h1>=2) {
+			return 20.00;
+		}
+		else {
+			return 10.00;
+		}
+	}
+	
 	
 	public String getName() {
 		return name;
@@ -27,12 +57,6 @@ public class Parking {
 	}
 	public void setModel(String model) {
 		this.model = model;
-	}
-	public Integer getLicense_plates() {
-		return license_plates;
-	}
-	public void setLicense_plates(Integer license_plates) {
-		this.license_plates = license_plates;
 	}
 
 	public Integer getHora() {
